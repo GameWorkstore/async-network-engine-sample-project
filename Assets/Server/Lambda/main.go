dabcd5a081f9e8db0f84252d2bd8470b3d71c485
Hanza Ru <woodimbr@gmail.com>
2021-02-13 11:48:32 -0300

    +First lines of Go Lambda function

--- Assets/Server/Lambda/main.go
+++ Assets/Server/Lambda/main.go
@@ -1,1 +1,35 @@
+package appfunction
 
+import (
+	"context"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// ImplementationFailure handles requests from API Gateway when function wasn't implemented properly
+func ImplementationFailure(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	var resp = events.APIGatewayProxyResponse{}
+	resp.Body = "Implementation failure, verify if main contains the name of function"
+	resp.StatusCode = 404
+
+	return resp, nil
+}
+
+func main() {
+
+	var functionName = os.Getenv("AWS_LAMBDA_FUNCTION_NAME")
+	switch functionName {
+	case "writedata":
+		lambda.Start(ImplementationFailure)
+		return
+	case "getdata":
+		lambda.Start(ImplementationFailure)
+		return
+	default:
+		lambda.Start(ImplementationFailure)
+		return
+	}
+}
