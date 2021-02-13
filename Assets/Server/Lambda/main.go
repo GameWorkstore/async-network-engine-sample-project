3ea55e312ad208d8bb45c899e9647b99114fc14c
Hanza Ru <woodimbr@gmail.com>
2021-02-13 13:33:34 -0300

    Update main.go

--- Assets/Server/Lambda/main.go
+++ Assets/Server/Lambda/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 
@@ -12,10 +13,32 @@ import (
 // ImplementationFailure handles requests from API Gateway when function wasn't implemented properly
 func ImplementationFailure(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	fmt.Println("Test")
-
 	var resp = events.APIGatewayProxyResponse{}
 	resp.Body = "Implementation failure, verify if main contains the name of function"
+	resp.StatusCode = 200
+
+	return resp, nil
+}
+
+// Request is struct
+type Request struct {
+	Name string `json:"name"`
+}
+
+// GetData is a function
+func GetData(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	var rqt Request
+	err := json.Unmarshal([]byte(request.Body), &rqt)
+	if err != nil {
+		var fail = events.APIGatewayProxyResponse{}
+		fail.Body = "Unmashal error"
+		fail.StatusCode = 200
+		return fail, nil
+	}
+
+	var resp = events.APIGatewayProxyResponse{}
+	resp.Body = "Welcome, " + rqt.Name
 	resp.StatusCode = 200
 
 	return resp, nil
@@ -29,8 +52,8 @@ func main() {
 	case "writedata":
 		lambda.Start(ImplementationFailure)
 		return
-	case "getdata":
-		lambda.Start(ImplementationFailure)
+	case "lksgetdata":
+		lambda.Start(GetData)
 		return
 	default:
 		lambda.Start(ImplementationFailure)
