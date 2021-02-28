7735378b86671285e1539e09a7131fce94b80427
Hanza Ru <woodimbr@gmail.com>
2021-02-28 11:33:32 -0300

    Update MyMono.cs

--- Assets/Scripts/MyMono.cs
+++ Assets/Scripts/MyMono.cs
@@ -1,20 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using Asyncnetworkengine;
 using Google.Protobuf;
+using ProtobufData;
 using UnityEngine;
 
 public class MyMono : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private const string GetEndpoint = "http";
+
     void Start()
     {
-        
+        AsyncNetworkEngine<GetUserRequest, GetUserResponse, GenericErrorResponse>.Cloud = AsyncNetworkEngineCloud.AWS;
+
+        var rqt = new GetUserRequest()
+        {
+            Id = "123456"
+        };
+
+        AsyncNetworkEngine<GetUserRequest, GetUserResponse, GenericErrorResponse>.Send(GetEndpoint, rqt, OnGetUserResponse);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnGetUserResponse(AsyncNetworkResult result, GetUserResponse response, GenericErrorResponse error)
     {
-        
+        Debug.Log("Result:" + result);
+        Debug.Log("Response:" + response != null? response.ToString() : "null" );
+        Debug.Log("Result:" + error != null? error.ToString() : "null");
     }
 }
